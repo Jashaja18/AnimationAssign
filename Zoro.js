f689b2be029559d00bf444fa163043ca52c4dfc4
Jashanpreet Jandu <jashanjandu23@gmail.com>
2023-01-08 20:01:50 -0800

    Done

--- Zoro.js
+++ Zoro.js
@@ -1,18 +1,18 @@
 class Zoro {
-  constructor(game){
-      this.game = game;
-      this.animation = new Animator(ASSET_MANAGER.getAsset("./img/Walk.png"), -1, 2, 48, 148, 8, .06);
-      this.x = 0;
-      this.y = 483;
-      this.speed = 200;
+  constructor(game) {
+    this.game = game;
+    this.animation = new Animator(ASSET_MANAGER.getAsset("./img/Walk.png"), -1, 2, 48, 148, 8, .06);
+    this.x = 0;
+    this.y = 483;
+    this.speed = 200;
   };
 
-  update(){
-      this.x += this.game.clockTick * this.speed;
-      if (this.x > 1024) this.x = -40;
+  update() {
+    this.x += this.game.clockTick * this.speed;
+    if (this.x > 1024) this.x = -40;
   };
 
-  draw(ctx){
-      this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+  draw(ctx) {
+    this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
   };
 }
