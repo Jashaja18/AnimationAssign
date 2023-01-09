69657ea147425460c6910759c9c120dc709decb4
Jashanpreet Jandu <jashanjandu23@gmail.com>
2023-01-08 20:07:39 -0800

    Done

--- World.js
+++ World.js
@@ -1,10 +1,9 @@
 class World {
   constructor(game) {
     this.game = game;
-    this.animation = new Animator(ASSET_MANAGER.getAsset("./img/Background.png"), 0, 0, 1024, 768, 1, 1);
+    this.animation = new Animator(ASSET_MANAGER.getAsset("./img/World.png"), 0, 0, 1024, 768, 1, 1, false, true);
     this.x = 0;
     this.y = 0;
-    this.speed = 0;
   };
 
   update() {
