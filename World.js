8be0a904b523b88f4642f5c9e985a96b7ca32ddf
Jashanpreet Jandu <jashanjandu23@gmail.com>
2023-01-08 20:05:42 -0800

    Done

--- World.js
+++ World.js
@@ -1,12 +1,18 @@
 class World {
-  constructor(game){
+  constructor(game) {
+    this.game = game;
+    this.animation = new Animator(ASSET_MANAGER.getAsset("./img/Background.png"), 0, 0, 1024, 768, 1, 1);
+    this.x = 0;
+    this.y = 0;
+    this.speed = 0;
   };
 
-  update(){
+  update() {
 
   };
 
-  draw(ctx){
-      ctx.drawImage(ASSET_MANAGER.getAsset("./img/Background.png"), 0, 0, 1024, 768);
-  }
+  draw(ctx) {
+    ctx.clearRect(0, 0, 1024, 768);
+    this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+  };
 }
